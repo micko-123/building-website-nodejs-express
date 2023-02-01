e5ab37b85cb07e118f157fe4706701d459d77bc0
Mickyas Asnake <60357226+micko-123@users.noreply.github.com>
2023-02-01 07:22:15 -0800

    routes added

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const router = require('./routes')
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'./views'))
+
+app.use(express.static(path.join(__dirname,'./static')))
+
+app.use('/', router())
+
+app.listen(port, ()=>{
+	console.log('server listening...')
+})
